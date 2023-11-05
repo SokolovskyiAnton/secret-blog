02b4de5fff72252b7b4bbcab34390136b24cb52d
docia <matiushenko107@gmail.com>
2023-11-05 22:28:56 +0200

    used BEM

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,58 +2,60 @@
 	<div class="login-wrapper">
 		<div class="login-container">
 			<div class="login-header">
-				<h1 class="login-title">Log In</h1>
+				<h1 class="login-header__title">Log In</h1>
 			</div>
-			<div class="input-box">
-				<input
-					type="text"
-					class="input-field"
-					placeholder="First name"
-					v-model.trim="firstName"
-					:class="{ errorBorder: shouldApplyErrorBorder('firstName') }"
-				/>
-			</div>
-			<span v-if="v$.firstName.$error" class="error-span">
-				{{ v$.firstName.$errors[0].$message }}
-			</span>
-			<div class="input-box">
-				<input
-					type="text"
-					class="input-field"
-					placeholder="Last name"
-					v-model.trim="lastName"
-					:class="{ errorBorder: shouldApplyErrorBorder('lastName') }"
-				/>
-			</div>
-			<span v-if="v$.lastName.$error" class="error-span">
-				{{ v$.lastName.$errors[0].$message }}
-			</span>
-			<div class="input-box">
-				<input
-					type="text"
-					class="input-field"
-					placeholder="Email"
-					v-model.trim="email"
-					:class="{ errorBorder: shouldApplyErrorBorder('email') }"
-				/>
-			</div>
-			<span v-if="v$.email.$error" class="error-span">
-				{{ v$.email.$errors[0].$message }}
-			</span>
-			<div class="button-arrow">
-				<button class="login-button button-blue" @click.prevent="logIn">
-					Log in
-				</button>
-			</div>
-			<div class="checkbox-terms-agree input-box">
-				<p class="terms-agree">
-					Don't have an account?
-					<span class="create-one"
-						><router-link class="plain-link" :to="{ name: 'SignupView' }"
-							>Create one</router-link
-						></span
-					>
-				</p>
+			<div class="login-form">
+				<div class="login-form__input-box">
+					<input
+						type="text"
+						class="login-form__input-field"
+						placeholder="First name"
+						v-model.trim="firstName"
+						:class="{ errorBorder: shouldApplyErrorBorder('firstName') }"
+					/>
+				</div>
+				<span v-if="v$.firstName.$error" class="login-form__error-span">
+					{{ v$.firstName.$errors[0].$message }}
+				</span>
+				<div class="login-form__input-box">
+					<input
+						type="text"
+						class="login-form__input-field"
+						placeholder="Last name"
+						v-model.trim="lastName"
+						:class="{ errorBorder: shouldApplyErrorBorder('lastName') }"
+					/>
+				</div>
+				<span v-if="v$.lastName.$error" class="login-form__error-span">
+					{{ v$.lastName.$errors[0].$message }}
+				</span>
+				<div class="login-form__input-box">
+					<input
+						type="text"
+						class="login-form__input-field"
+						placeholder="Email"
+						v-model.trim="email"
+						:class="{ errorBorder: shouldApplyErrorBorder('email') }"
+					/>
+				</div>
+				<span v-if="v$.email.$error" class="login-form__error-span">
+					{{ v$.email.$errors[0].$message }}
+				</span>
+				<div class="login-form__button-arrow">
+					<button class="login-form__button login-form__button--blue" @click.prevent="logIn">
+						Log in
+					</button>
+				</div>
+				<div class="login-footer login-form__input-box">
+					<p>
+						Don't have an account?
+						<span
+							><router-link class="login-footer__plain-link" :to="{ name: 'SignupView' }"
+								>Create one</router-link
+							></span
+						>
+					</p>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -93,7 +95,6 @@ export default {
 		logIn() {
 			this.v$.$validate();
 			if (!this.v$.$error) {
-				alert("You successfully logged in!");
 				this.isInvalid = false;
 			} else {
 				this.isInvalid = true;
--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -2,86 +2,98 @@
 	<div class="signup-wrapper">
 		<div class="signup-container">
 			<div class="signup-header">
-				<h1 class="create-account-title">Create account</h1>
-				<span class="create-account-subtitle"
+				<h1 class="signup-header__title">Create account</h1>
+				<span class="signup-header__subtitle"
 					>Already have an account?
-					<router-link class="plain-link" :to="{ name: 'LoginView' }"
+					<router-link
+						class="signup-header__plain-link"
+						:to="{ name: 'LoginView' }"
 						>Log In</router-link
 					>
 				</span>
 			</div>
-			<div class="input-box">
-				<input
-					type="text"
-					class="input-field"
-					placeholder="Username"
-					v-model.trim="userName"
-					:class="{ errorBorder: shouldApplyErrorBorder('userName') }"
-				/>
-			</div>
-			<span v-if="v$.userName.$error" class="error-span">
-				{{ v$.userName.$errors[0].$message }}
-			</span>
-			<div class="input-box first-last-name-input">
-				<input
-					type="text"
-					class="input-field firstname-input"
-					placeholder="First name"
-					v-model.trim="firstName"
-					:class="{ errorBorder: shouldApplyErrorBorder('firstName') }"
-				/>
-
-				<input
-					type="text"
-					class="input-field"
-					placeholder="Last name"
-					v-model.trim="lastName"
-					:class="{ errorBorder: shouldApplyErrorBorder('lastName') }"
-				/>
-			</div>
-			<div
-				class="error-span-last-first-name"
-				v-if="v$.firstName.$error || v$.lastName.$error"
-			>
-				<span v-if="v$.firstName.$error" class="error-span">
-					{{ v$.firstName.$errors[0].$message }}
+			<div class="signup-form">
+				<div class="signup-form__input-box">
+					<input
+						type="text"
+						class="signup-form__input-field"
+						placeholder="Username"
+						v-model.trim="userName"
+						:class="{ errorBorder: shouldApplyErrorBorder('userName') }"
+					/>
+				</div>
+				<span v-if="v$.userName.$error" class="signup-form__error-span">
+					{{ v$.userName.$errors[0].$message }}
 				</span>
-				<span v-if="v$.lastName.$error" class="error-span">
-					{{ v$.lastName.$errors[0].$message }}
+				<div class="signup-form__input-box signup-form__input-box--first-last-name-input">
+					<div class="signup-form__input-error-box">
+						<input
+							type="text"
+							class="signup-form__input-field signup-form__input-field--firstname-input"
+							placeholder="First name"
+							v-model.trim="firstName"
+							:class="{ errorBorder: shouldApplyErrorBorder('firstName') }"
+						/>
+						<span
+							v-if="v$.firstName.$error"
+							class="signup-form__error-span signup-form__error-span--first-last-name"
+						>
+							{{ v$.firstName.$errors[0].$message }}
+						</span>
+					</div>
+					<div class="signup-form__input-error-box">
+						<input
+							type="text"
+							class="signup-form__input-field signup-form__input-field--firstname-input"
+							placeholder="Last name"
+							v-model.trim="lastName"
+							:class="{ errorBorder: shouldApplyErrorBorder('lastName') }"
+						/>
+						<span
+							v-if="v$.lastName.$error"
+							class="signup-form__error-span signup-form__error-span--first-last-name"
+						>
+							{{ v$.lastName.$errors[0].$message }}
+						</span>
+					</div>
+				</div>
+				<div class="signup-form__input-box">
+					<input
+						type="text"
+						class="signup-form__input-field"
+						placeholder="Email"
+						v-model.trim="email"
+						:class="{ errorBorder: shouldApplyErrorBorder('email') }"
+					/>
+				</div>
+				<span v-if="v$.email.$error" class="signup-form__error-span">
+					{{ v$.email.$errors[0].$message }}
+				</span>
+				<div class="signup-form__button--arrow">
+					<button class="signup-form__button signup-form__button--blue" @click.prevent="signUp">
+						Sign up
+					</button>
+					<img
+						src="../assets/images/arrow.svg"
+						alt="Arrow to the right"
+						class="signup-form__button--arrow-right-icon"
+					/>
+				</div>
+				<div class="signup-footer signup-form__input-box">
+					<input
+						type="checkbox"
+						class="signup-footer__checkbox-terms"
+						v-model.trim="checked"
+					/>
+					<p class="signup-footer__terms-agree">
+						I have read and agree to
+						<span class="signup-footer__terms-of-service">Terms of Service</span>
+					</p>
+				</div>
+				<span v-if="v$.checked.$error" class="signup-form__error-span">
+					Please read and agree to Terms of Service
 				</span>
 			</div>
-			<div class="input-box">
-				<input
-					type="text"
-					class="input-field"
-					placeholder="Email"
-					v-model.trim="email"
-					:class="{ errorBorder: shouldApplyErrorBorder('email') }"
-				/>
-			</div>
-			<span v-if="v$.email.$error" class="error-span">
-				{{ v$.email.$errors[0].$message }}
-			</span>
-			<div class="button-arrow">
-				<button class="signup-button button-blue" @click.prevent="signUp">
-					Sign up
-				</button>
-				<img
-					src="../assets/images/arrow.svg"
-					alt="Arrow to the right"
-					class="arrow-right-icon"
-				/>
-			</div>
-			<div class="checkbox-terms-agree input-box">
-				<input type="checkbox" class="checkbox-terms" v-model.trim="checked" />
-				<p class="terms-agree">
-					I have read and agree to
-					<span class="terms-of-service">Terms of Service</span>
-				</p>
-			</div>
-			<span v-if="v$.checked.$error" class="error-span">
-				Please read and agree to Terms of Service
-			</span>
 		</div>
 	</div>
 </template>
@@ -126,7 +138,6 @@ export default {
 			this.v$.$validate();
 			if (!this.v$.$error) {
 				this.isInvalid = false;
-				alert("You successfully signed up!");
 			} else {
 				this.isInvalid = true;
 			}
