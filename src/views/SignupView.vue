0664d11bf0fb04e62748ece34e612b2ad88aa25a
Anton Sokolovskyi <sokolovskii.anton@gmail.com>
2024-01-05 23:21:06 +0200

    clean up

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -3,53 +3,45 @@
 		<div class="signup__content">
 			<div class="signup__content__header">
 				<h1 class="signup__content__header__title">Create account</h1>
-				<span class="signup__content__header__subtitle"
-					>Already have an account?
-					<router-link
-						class="signup__content__header__subtitle__link"
-						:to="{ name: 'LoginView' }"
-						>Log In</router-link
-					>
-				</span>
 			</div>
 			<form @submit.prevent="handleSubmit" class="signup__content__form">
-				<div class="signup__content__form__fullName">
-					<div class="signup__content__form__fullName__firstName">
+				<div class="flex">
+					<div class="signup__content__form__item mr-8">
 						<input
 							type="text"
-							class="signup__content__form__fullName__firstName__input"
+							class="signup__content__form__item__input"
 							placeholder="First name"
 							v-model.trim="form.firstName"
 							:class="{
-								'signup__content__form__fullName__firstName__input-error':
+								'signup__content__form__item__input-error':
 									v$.form.firstName.$error,
 							}"
 						/>
 						<span
 							v-if="v$.form.firstName.$error"
-							class="signup__content__form__fullName__firstName__error"
+							class="signup__content__form__item__error"
 						>
 							{{ v$.form.firstName.$errors[0].$message }}
 						</span>
 					</div>
-					<div class="signup__content__form__fullName__lastName">
-						<input
-							type="text"
-							class="signup__content__form__fullName__lastName__input"
-							placeholder="Last name"
-							v-model.trim="form.lastName"
-							:class="{
-								'signup__content__form__fullName__lastName__input-error':
-									v$.form.lastName.$error,
+          <div class="signup__content__form__item ml-8">
+            <input
+                type="text"
+                class="signup__content__form__item__input"
+                placeholder="First name"
+                v-model.trim="form.lastName"
+                :class="{
+								'signup__content__form__item__input-error':
+									v$.form.firstName.$error,
 							}"
-						/>
-						<span
-							v-if="v$.form.lastName.$error"
-							class="signup__content__form__fullName__lastName__error"
-						>
+            />
+            <span
+                v-if="v$.form.lastName.$error"
+                class="signup__content__form__item__error"
+            >
 							{{ v$.form.lastName.$errors[0].$message }}
 						</span>
-					</div>
+          </div>
 				</div>
 				<div class="signup__content__form__item">
 					<input
@@ -101,27 +93,17 @@
 						class="signup__content__form__submit__arrow"
 					/>
 				</div>
-				<div class="signup__content__form__footer">
-					<div class="signup__content__form__footer__item">
-						<input
-							type="checkbox"
-							class="signup__content__form__footer__item__checkbox"
-							v-model.trim="checked"
-						/>
-						<p class="signup__content__form__footer__item__terms">
-							I have read and agree to
-							<span class="signup__content__form__footer__terms__link"
-								>Terms of Service</span
-							>
-						</p>
-					</div>
-					<span
-						v-if="v$.checked.$error"
-						class="signup__content__form__footer__error"
-					>
-						Please read and agree to Terms of Service
-					</span>
-				</div>
+        <div class="signup__content__form__footer">
+          <p>
+            Do you have an account?
+            <router-link
+                class="login__content__form__footer__link"
+                :to="{ name: 'LoginView' }"
+            >
+              Login
+            </router-link>
+          </p>
+        </div>
 			</form>
 		</div>
 	</div>
@@ -145,7 +127,6 @@ export default {
 				email: "",
 				password: "",
 			},
-			checked: "",
 		};
 	},
 	validations() {
@@ -155,8 +136,7 @@ export default {
 				lastName: { required, minLength: minLength(4) },
 				email: { required, email },
 				password: { required },
-			},
-			checked: { sameAs: sameAs(true) },
+			}
 		};
 	},
 	methods: {
@@ -179,31 +159,6 @@ export default {
 </script>
 
 <style lang="scss">
-@mixin inputStyles {
-	width: 100%;
-	height: 46px;
-	padding: 0 12px;
-	border-radius: 12px;
-	border: none;
-	background-color: var(--input-background-color);
-	box-sizing: border-box;
-}
-
-@mixin errorStyles {
-	color: var(--red-color);
-	font-size: 16px;
-	display: flex;
-	margin-top: 8px;
-}
-
-@mixin borderStyle {
-	border: 1px solid var(--red-color);
-}
-
-@mixin topMargin {
-	margin-top: 16px;
-}
-
 .signup {
 	display: flex;
 	justify-content: center;
@@ -214,7 +169,7 @@ export default {
 
 	&__content {
 		border: none;
-		border-radius: 24px;
+		border-radius: var(--grid-24);
 		padding: 72px 96px 96px;
 		background-color: var(--content-color);
 
@@ -228,50 +183,31 @@ export default {
 				font-size: 36px;
 				font-weight: bold;
 			}
-			&__subtitle {
-				&__link {
-					text-decoration: none;
-					color: var(--blue-color);
-					cursor: pointer;
-				}
-			}
 		}
 
 		&__form {
-			&__fullName {
-				display: flex;
-				margin-top: 16px;
-
-				&__firstName,
-				&__lastName {
-					&__input {
-						@include inputStyles;
-						&:placeholder-shown {
-							font-style: italic;
-						}
-						&-error {
-							@include borderStyle;
-						}
-					}
-					&__error {
-						@include errorStyles;
-					}
-				}
-			}
-
 			&__item {
-				@include topMargin;
+				margin-top: var(--grid-12);
 				&__input {
-					@include inputStyles;
+          width: 100%;
+          padding: var(--grid-12);
+          border-radius: var(--grid-12);
+          border: none;
+          background-color: var(--input-background-color);
+          box-sizing: border-box;
+
 					&:placeholder-shown {
 						font-style: italic;
 					}
 					&-error {
-						@include borderStyle;
+            border: 1px solid var(--red-color);
 					}
 				}
 				&__error {
-					@include errorStyles;
+          color: var(--red-color);
+          font-size: var(--grid-16);
+          display: flex;
+          margin-top: var(--grid-8);
 				}
 			}
 
@@ -295,29 +231,15 @@ export default {
 					left: -12px;
 				}
 			}
-
-			&__footer {
-				@include topMargin;
-
-				&__item {
-					display: flex;
-
-					&__checkbox {
-						margin-right: 4px;
-						&:hover {
-							cursor: pointer;
-						}
-					}
-				}
-				&__terms {
-					&__link {
-						color: var(--blue-color);
-					}
-				}
-				&__error {
-					@include errorStyles;
-				}
-			}
+      &__footer {
+        margin-top: 16px;
+        text-align: center;
+        &__link {
+          text-decoration: none;
+          color: var(--blue-color);
+          cursor: pointer;
+        }
+      }
 		}
 	}
 }
