f639402ab64378d074028bb0484d5bd0033f010c
docia <matiushenko107@gmail.com>
2024-01-25 13:26:12 +0200

    updated PostFormComponent tests

--- src/components/__tests__/PostFormComponent.spec.js
+++ src/components/__tests__/PostFormComponent.spec.js
@@ -1,42 +1,57 @@
-import { describe, it, expect, forEach, beforeEach, vitest } from "vitest";
+import { describe, it, expect, beforeEach, vitest, vi, test } from "vitest";
+import { mount, flushPromises } from "@vue/test-utils";
 import PostFormComponent from "@/components/PostFormComponent.vue";
-import { usePostStore } from "../../stores/postStore";
-import { flushPromises, mount } from "@vue/test-utils";
 import { createTestingPinia } from "@pinia/testing";
-import { VuelidatePlugin } from "@vuelidate/core";
+import { nextTick } from "vue";
+import { usePostStore } from "@/stores/postStore";
+import { useFileStore } from "@/stores/fileStore";
 
 describe("PostFormComponent", () => {
-	let wrapper = null;
-
+	let wrapper;
 	beforeEach(() => {
 		wrapper = mount(PostFormComponent, {
 			global: {
-				plugins: [
-					createTestingPinia({
-						createSpy: vitest.fn,
-					}),
-					VuelidatePlugin,
-				],
+				plugins: [createTestingPinia()],
 			},
 		});
 	});
 
-	it("if validation works", async () => {
-		const postStore = usePostStore();
+	it("check component validation", async () => {
+		await wrapper.find("button").trigger("submit");
 
-		const button = wrapper.find("button");
-		
-        await button.trigger("submit");
+		expect(wrapper.find("#postFormInput2").classes()).toContain(
+			"InputComponent__error"
+		);
+    });
+    
+	it("check creating post", async () => {
+        const postStore = usePostStore();
+        
+		await wrapper.find("#postFormInput1").setValue("Hello");
+		await wrapper.find("#postFormInput2").setValue("Hello Hello Hello Hello");
+		await wrapper.find("#postFormInput3").setValue("Hello Hello Hello Hello");
+		await wrapper.find("button").trigger("submit");
+        await flushPromises();
+        
+		expect(postStore.createPost).toHaveBeenCalled();
+		expect(postStore.getPosts).toHaveBeenCalled();
+		expect(wrapper.emitted()).toHaveProperty("close");
+    });
+    
+	it("check upload image", async () => {
+		const fileStore = useFileStore();
+		const inputFile = wrapper.find('input[type="file"]');
+		const fileMock = new File(["(file content)"], "test-file.txt", {
+			type: "text/plain",
+		});
 
-		// Wait for Vue to process the click event and update the component state
-		await wrapper.vm.$nextTick();
+		expect(inputFile.exists()).toBe(true);
 
-		await flushPromises();
+		Object.defineProperty(inputFile.element, "files", {
+			value: [fileMock],
+		});
+		await inputFile.trigger("change");
 
-		const inputComponentError = wrapper.find("#postFormInput1");
-
-		const classes = inputComponentError.classes();
-
-		expect(classes).toContain("InputComponent__error");
+		expect(fileStore.uploadImage).toHaveBeenCalledTimes(1);
 	});
 });
