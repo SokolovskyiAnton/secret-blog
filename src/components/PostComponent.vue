5ddb7a72013bd2e1e3b342a1f760aca3e1700486
Anton Sokolovskyi <sokolovskii.anton94@gmail.com>
2024-02-04 23:11:25 +0200

    clean up PostComponent

--- src/components/PostComponent.vue
+++ src/components/PostComponent.vue
@@ -1,78 +1,73 @@
 <template>
 	<div class="post">
-		<div class="post__content">
-			<div
-				class="post__content__main"
-				@click.prevent="toPostDetailing"
-				title="Click anywhere to expand the post"
-			>
-				<!--EDIT BUTTON-->
-				<!-- <div class="post__content__main__dropdown">
-					<button
-						class="post__content__main__dropdown__btn"
-						@click.prevent="openDropdpown"
-					>
-						<img
-							src="../assets/images/more-icon.svg"
-							alt="more icon"
-							class="post__content__main__dropdown__btn__icon"
-						/>
-					</button>
-					<div
-						class="post__content__main__dropdown__content"
-						:class="{
-							post__content__main__dropdown__content__visible:
-								isDropdownVisible,
-						}"
-					>
-						<div class="post__content__main__dropdown__content__editBtn">
-							<img
-								src="../assets/images/edit-post-icon.svg"
-								alt="edit post icon"
-							/>
-							<a href="#" @click.prevent="openModal"> Edit Post</a>
-						</div>
-						<div class="post__content__main__dropdown__content__deleteBtn">
-							<img
-								src="../assets/images/trash-icon.svg"
-								alt="delete post icon"
-							/>
-							<a href="#" @click.prevent="openModal"> Delete Post</a>
-						</div>
-					</div>
-				</div> -->
-				<div class="post__content__main__picture">
-					<img
-						:src="postPicture"
-						alt="mountains"
-						class="post__content__main__picture__img"
-					/>
-				</div>
+    <div
+        class="post__main"
+        @click.prevent="toPostDetailing"
+        title="Click anywhere to expand the post"
+    >
+      <!--EDIT BUTTON-->
+      <!-- <div class="post__content__main__dropdown">
+        <button
+          class="post__content__main__dropdown__btn"
+          @click.prevent="openDropdpown"
+        >
+          <img
+            src="../assets/images/more-icon.svg"
+            alt="more icon"
+            class="post__content__main__dropdown__btn__icon"
+          />
+        </button>
+        <div
+          class="post__content__main__dropdown__content"
+          :class="{
+            post__content__main__dropdown__content__visible:
+              isDropdownVisible,
+          }"
+        >
+          <div class="post__content__main__dropdown__content__editBtn">
+            <img
+              src="../assets/images/edit-post-icon.svg"
+              alt="edit post icon"
+            />
+            <a href="#" @click.prevent="openModal"> Edit Post</a>
+          </div>
+          <div class="post__content__main__dropdown__content__deleteBtn">
+            <img
+              src="../assets/images/trash-icon.svg"
+              alt="delete post icon"
+            />
+            <a href="#" @click.prevent="openModal"> Delete Post</a>
+          </div>
+        </div>
+      </div> -->
+      <div class="post__main__picture">
+        <img
+            :src="postPicture"
+            alt="mountains"
+            class="post__main__picture__img"
+        />
+      </div>
 
-				<h1>{{ post.title }}</h1>
-				<div class="post__content__main__description">
-					<p>{{ post.description }}</p>
-				</div>
-			</div>
+      <h1 class="mb-8">{{ post.title }}</h1>
+      <p class="mb-8">{{ post.description }}</p>
+    </div>
 
-			<div class="post__content__footer">
-				<small>{{ formattedDate }}</small>
-				<div class="post__content__footer__likeBtn">
-					<button
-						class="post__content__footer__likeBtn__toggle"
-						:disabled="isLoading"
-						@click.prevent="handleLike(post._id)"
-					>
-						<img
-							:src="likeIcon"
-							alt="like icon"
-							class="post__content__footer__likeBtn__toggle__likeIcon"
-						/>
-					</button>
-					<div>{{ count }}</div>
-				</div>
-			</div>
-		</div>
+    <div class="post__footer">
+      <small>{{ formattedDate }}</small>
+      <div class="post__footer__likeBtn">
+        <button
+            class="post__footer__likeBtn__toggle"
+            :disabled="isLoading"
+            @click.prevent="handleLike(post._id)"
+        >
+          <img
+              :src="likeIcon"
+              alt="like icon"
+          />
+        </button>
+        <div>{{ count }}</div>
+      </div>
+    </div>
 	</div>
 </template>
 
@@ -167,118 +162,101 @@ const toPostDetailing = (postId) => {
 
 <style lang="scss">
 .post {
-	box-shadow: 10px 10px 5px var(--black-color-8);
-	padding: 14px;
-	border-radius: 12px;
-	&__content {
-		&__main {
-			display: flex;
-			flex-direction: column;
-			cursor: pointer;
-			&__dropdown {
-				width: 40px;
-				height: 40px;
-				margin-left: auto;
-				position: relative;
-				display: inline-block;
-				&__btn {
-					border-radius: 90%;
-					border: none;
-					background-color: transparent;
-					padding: 4px 4px 0;
-					&:hover {
-						background-color: var(--black-color-7);
-						cursor: pointer;
-					}
-				}
+	box-shadow: 0 0 5px var(--black-color-8);
+	border-radius: var(--grid-12);
+  padding: var(--grid-12);
+  overflow: hidden;
+  &__main {
+    display: flex;
+    flex-direction: column;
+    cursor: pointer;
+    &__dropdown {
+      width: 40px;
+      height: 40px;
+      margin-left: auto;
+      position: relative;
+      display: inline-block;
+      &__btn {
+        border-radius: 90%;
+        border: none;
+        background-color: transparent;
+        padding: 4px 4px 0;
+        &:hover {
+          background-color: var(--black-color-7);
+          cursor: pointer;
+        }
+      }
 
-				&__content {
-					display: none;
-					position: absolute;
-					right: 0;
-					background-color: #f9f9f9;
-					min-width: 160px;
-					z-index: 1;
-					&__editBtn,
-					&__deleteBtn {
-						display: flex;
-						padding: 0 8px 0;
-						&:hover {
-							background-color: #f1f1f1;
-							border-radius: 8px;
-							cursor: pointer;
-						}
-					}
+      &__content {
+        display: none;
+        position: absolute;
+        right: 0;
+        background-color: #f9f9f9;
+        min-width: 160px;
+        z-index: 1;
+        &__editBtn,
+        &__deleteBtn {
+          display: flex;
+          padding: 0 8px 0;
+          &:hover {
+            background-color: #f1f1f1;
+            border-radius: 8px;
+            cursor: pointer;
+          }
+        }
 
-					&__visible {
-						display: block;
-						border-radius: 8px;
-					}
+        &__visible {
+          display: block;
+          border-radius: 8px;
+        }
 
-					a {
-						color: var(--black-color-2);
-						padding: 12px 14px;
-						text-decoration: none;
-						display: block;
-					}
-				}
-			}
-			&__picture {
-				// width: 352px;
-				max-width: 100%;
-				height: 360px;
-				// max-height: 100%;
-				// padding: 12px 0;
-				// max-height: 360px; /* Set a maximum height for the image container */
-				// overflow: hidden; /* Hide any overflow content */
-				// width: 100%;
-				// height: 100%;
-				position: relative;
-				padding: 12px 0;
-				&__img {
-					// display: block;
-					// height: auto;
-					// object-fit: cover;
-					width: 100%;
-					// max-height: 360px;
-					height: 100%;
-					// max-width: 100%; /* Ensure image doesn't exceed the width of its container */
-					// height: auto; /* Maintain aspect ratio */
-					object-fit: cover;
-				}
-			}
+        a {
+          color: var(--black-color-2);
+          padding: 12px 14px;
+          text-decoration: none;
+          display: block;
+        }
+      }
+    }
+    &__picture {
+      height: 300px;
+      overflow: hidden;
+      position: relative;
+      border-radius: var(--grid-12);
+      margin-bottom: var(--grid-12);
+      &__img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
 
-			h1,
-			p {
-				white-space: nowrap;
-				overflow: hidden;
-				text-overflow: ellipsis;
-			}
-
-			&__description {
-				margin: 16px 0 16px;
-			}
-		}
-		&__footer {
-			display: flex;
-			justify-content: space-between;
-			align-items: center;
-			&__likeBtn {
-				display: flex;
-				align-items: center;
-				gap: 5px;
-				&__toggle {
-					background-color: transparent;
-					border: none;
-					padding: 0;
-					align-items: center;
-					display: flex;
-					&:hover {
-						cursor: pointer;
-					}
-				}
-			}
-		}
-	}
+    h1,
+    p {
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+  }
+  &__footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    &__likeBtn {
+      display: flex;
+      align-items: center;
+      gap: 5px;
+      &__toggle {
+        background-color: transparent;
+        border: none;
+        padding: 0;
+        align-items: center;
+        display: flex;
+        &:hover {
+          cursor: pointer;
+        }
+      }
+    }
+  }
 }
 </style>
